26b5d97024d426b8661965c1d2173adf86b4aef1
nijatkazimli <nctkzmli@gmail.com>
2023-10-08 17:32:22 +0200

    third task

--- FirstLab/Ex1.R
+++ FirstLab/Ex1.R
@@ -8,6 +8,7 @@ c <- seq(1, 99, by = 2)
 d <- seq(100, 1)
 e <- seq(100, -5)
 
+
 # Exercise 1.2a
 i1 <- c(5, 5, 6);   typeof(i1)  # double
 i2 <- seq(1, 50);   typeof(i2)  # integer
@@ -41,3 +42,24 @@ print_nested_list <- function(my_list, indent = 0) {
 # the function is for printing the list in a nice way (just cosmetics...)
 
 print_nested_list(myList)
+
+
+# Exercise 1.3
+a <- c("this", "is", "CS", "lab")
+b <- c("and", "we", "start", "today")
+
+taskA <- paste(a, collapse = " ")
+taskB <- paste(a, collapse = ".")
+taskC <- paste(a, b, sep = " ")
+taskD <- paste(a, b, sep = "|")
+taskE <- paste(a, b, collapse = " ")
+taskF <- paste(a, b, sep = "|", collapse = " ")
+taskG <- paste(paste(a, collapse = " "), paste(b, collapse = " "))
+
+b <- b[2:length(b)]
+taskH <- paste(paste(a, collapse = " "), paste(b, collapse = " "), sep = ", ")
+
+# sep param - takes a elements from each vector a, b, and creates a new element 
+#                                                           having a separator
+# collapse param - the same but creates one string which contains all new 
+#                                                                     elements
