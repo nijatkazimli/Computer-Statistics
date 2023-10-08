8a60e406dfc710955c06936f0fd36aa45ef14451
nijatkazimli <nctkzmli@gmail.com>
2023-10-08 16:38:07 +0200

    first task

--- FirstLab/Ex1.R
+++ FirstLab/Ex1.R
@@ -1,1 +1,9 @@
+rm(list = ls()) # removes all environment variables
+
 # Exercise set 1
+# Exercise 1.1
+a <- c(1L, 3L ,4L)
+b <- seq(1, 100)
+c <- seq(1, 99, by = 2)
+d <- seq(100, 1)
+e <- seq(100, -5)
