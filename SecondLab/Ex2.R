1b1a05c2e9a91085a91ce322788e4d3bbb9243ba
nijatkazimliPG <kazimli.n@pg.com>
2023-10-15 17:58:25 +0200

    added two tasks of exercise set 2

---
+++ SecondLab/Ex2.R
@@ -0,0 +1,41 @@
+rm(list = ls())
+
+# Exercise set 2
+# Exercise 2.1
+
+data <- c("m", "l", "l", "h", "h", "l", "h", "m", "h", "l")
+taskA <- "Categorical (Qualitative) Ordinal"
+
+taskB <- factor(data, levels = c("l", "m", "h"), 
+                labels = c("low", "middle", "high"), ordered = TRUE)
+print(taskB)
+
+taskC <- table(taskB)
+print(taskC)
+
+
+abs_freq <- table(taskB)
+rel_freq <- prop.table(abs_freq)
+cum_abs_freq <- cumsum(abs_freq)
+cum_rel_freq <- cumsum(rel_freq)
+
+taskD <- data.frame(
+  Class = levels(taskB),
+  Abs_Freq = as.numeric(abs_freq),
+  Rel_Freq = as.numeric(rel_freq),
+  Cum_Abs_Freq = cum_abs_freq,
+  Cum_Rel_Freq = cum_rel_freq
+)
+View(taskD)
+
+taskEi <- barplot(taskC, main = "Barplot of data", xlab = "Factor levels",
+                  ylab = "Frequency")
+taskEii <- pie(taskC, main = "Pie chart of data")
+
+
+# Exercise 2.2
+x <- data.frame(mtcars)
+task <- as.data.frame(as.numeric(table(x$cyl)))
+colnames(task) <- "Freuency"
+rownames(task) <- paste(as.data.frame(table(x$cyl))$Var1, "cylinders")
+View(task)
