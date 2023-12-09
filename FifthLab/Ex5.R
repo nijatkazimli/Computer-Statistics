e9a2199411fc74b64dcd8864116530779665c291
nijatkazimli <nctkzmli@gmail.com>
2023-12-09 13:48:54 +0100

    tasks

--- FifthLab/Ex5.R
+++ FifthLab/Ex5.R
@@ -50,3 +50,39 @@ alpha <- 0.1
 a <- qchisq(1 - alpha / 2, sample_size - 1)
 b <- qchisq(alpha / 2, sample_size - 1)
 (ci <- c((sample_size - 1)^2 / a, (sample_size - 1)^2 / b))
+
+
+# Exercise 5.3
+sample_size <- 25
+sample_sum <- 1508
+sample_square_sum <- 95628
+sample_mean <- sample_sum / sample_size
+
+# a
+known_variance <- 225
+known_sd <- sqrt(225)
+alpha <- 0.05
+z_score <- qnorm(1 - alpha / 2)
+(ci <- c(sample_mean - known_sd / sqrt(sample_size) * z_score,
+         sample_mean + known_sd / sqrt(sample_size) * z_score))
+rm(list = ls())
+
+# b
+# refer to my note for the below formula
+sample_variance <- (sample_square_sum - 2*sample_mean*sample_sum + sample_size*sample_mean^2) / (sample_size - 1)
+sample_standard_deviation <- sqrt(sample_variance)
+alpha <- 0.05
+t_score <- qt(1 - alpha / 2, sample_size - 1)
+(ci <- c(sample_mean - sample_standard_deviation / sqrt(sample_size) * t_score,
+         sample_mean + sample_standard_deviation / sqrt(sample_size) * t_score))
+rm(list = ls())
+
+# c
+sample_variance <- (sample_square_sum - 2*sample_mean*sample_sum + sample_size*sample_mean^2) / (sample_size - 1)
+sample_standard_deviation <- sqrt(sample_variance)
+alpha <- 0.10
+t_score <- qt(1 - alpha / 2, sample_size - 1)
+(ci <- c(sample_mean - sample_standard_deviation / sqrt(sample_size) * t_score,
+         sample_mean + sample_standard_deviation / sqrt(sample_size) * t_score))
+# it is narrower
+rm(list = ls())
